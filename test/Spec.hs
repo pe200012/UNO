b119c0da393f9aa613748061ad2412d5d120b8e2
pe200012 <1326263755@qq.com>
2020-09-27 16:22:18 +0800

    indent

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE StandaloneDeriving         #-}
 {-# LANGUAGE TypeApplications           #-}
 {-# LANGUAGE ViewPatterns               #-}
+
 module Main where
 
 import           Control.Eff
@@ -26,121 +27,192 @@ import           Test.QuickCheck         (Arbitrary (..), Property,
 import           Test.QuickCheck.Monadic (monadicIO)
 
 instance Arbitrary Kind where
-    arbitrary = do
-        n <- Number <$> chooseAny
-        s <- Spell <$> chooseAny
-        elements [n, s]
+  arbitrary = do
+    n <- Number <$> chooseAny
+    s <- Spell <$> chooseAny
+    elements [n, s]
 
 instance Arbitrary (Card Identity) where
-    arbitrary = Card <$> (return <$> chooseAny) <*> arbitrary
+  arbitrary = Card <$> (return <$> chooseAny) <*> arbitrary
 
 instance Arbitrary (Card Maybe) where
-    arbitrary = do
-        normalCard <- Card <$> (Just <$> chooseAny) <*> (suchThat arbitrary ((&&) . (/= Spell Universal) <*> (/= Spell Draw4)))
-        specialCard <- Card Nothing <$> (suchThat arbitrary ((||) . (== Spell Universal) <*> (== Spell Draw4)))
-        elements [normalCard, specialCard]
+  arbitrary = do
+    normalCard <-
+      Card <$> (Just <$> chooseAny) <*>
+      (suchThat arbitrary ((&&) . (/= Spell Universal) <*> (/= Spell Draw4)))
+    specialCard <-
+      Card Nothing <$>
+      (suchThat arbitrary ((||) . (== Spell Universal) <*> (== Spell Draw4)))
+    elements [normalCard, specialCard]
 
 instance Arbitrary Player where
-    arbitrary = Player <$> (fmap CardNew <$> suchThat arbitrary ((< 30) . length)) <*> arbitrary
+  arbitrary =
+    Player <$> (fmap CardNew <$> suchThat arbitrary ((< 30) . length)) <*>
+    arbitrary
 
-newtype SufficientPile = SufficientPile Pile deriving Show
+newtype SufficientPile =
+  SufficientPile Pile
+  deriving (Show)
 
 instance Arbitrary SufficientPile where
-    arbitrary = chooseInt (4, 108) >>= \n -> SufficientPile <$> (vectorOf n (CardNew <$> arbitrary))
+  arbitrary =
+    chooseInt (4, 108) >>= \n ->
+      SufficientPile <$> (vectorOf n (CardNew <$> arbitrary))
 
-newtype AnyPileTest = AnyPileTest Pile deriving Show
+newtype AnyPileTest =
+  AnyPileTest Pile
+  deriving (Show)
 
 instance Arbitrary AnyPileTest where
-    arbitrary = AnyPileTest <$> suchThat (listOf (CardNew <$> arbitrary)) ((<= 108) . length)
+  arbitrary =
+    AnyPileTest <$>
+    suchThat (listOf (CardNew <$> arbitrary)) ((<= 108) . length)
 
-newtype CapablePlayer = CapablePlayer Player deriving Show
+newtype CapablePlayer =
+  CapablePlayer Player
+  deriving (Show)
 
 instance Arbitrary CapablePlayer where
-    arbitrary = CapablePlayer <$> (Player <$> suchThat (listOf (CardNew <$> arbitrary)) ((> 0) . length) <*> suchThat arbitrarySizedNatural (> 1))
+  arbitrary =
+    CapablePlayer <$>
+    (Player <$> suchThat (listOf (CardNew <$> arbitrary)) ((> 0) . length) <*>
+     suchThat arbitrarySizedNatural (> 1))
 
-newtype IncapablePlayer = IncapablePlayer Player deriving Show
+newtype IncapablePlayer =
+  IncapablePlayer Player
+  deriving (Show)
 
 instance Arbitrary IncapablePlayer where
-    arbitrary = IncapablePlayer <$> Player [] <$> arbitrarySizedNatural
+  arbitrary = IncapablePlayer <$> Player [] <$> arbitrarySizedNatural
 
-newtype AnyPlayer = AnyPlayer Player deriving Show
+newtype AnyPlayer =
+  AnyPlayer Player
+  deriving (Show)
 
 instance Arbitrary AnyPlayer where
-    arbitrary = AnyPlayer <$> (Player <$> listOf (CardNew <$> arbitrary) <*> suchThat arbitrarySizedNatural (> 1))
+  arbitrary =
+    AnyPlayer <$>
+    (Player <$> listOf (CardNew <$> arbitrary) <*>
+     suchThat arbitrarySizedNatural (> 1))
 
-prop_drawsCorrectlyDrawNCardsIntoPlayerCandidates :: SufficientPile -> Player -> Property
-prop_drawsCorrectlyDrawNCardsIntoPlayerCandidates (SufficientPile pile) player = monadicIO $ do
+prop_drawsCorrectlyDrawNCardsIntoPlayerCandidates ::
+     SufficientPile -> Player -> Property
+prop_drawsCorrectlyDrawNCardsIntoPlayerCandidates (SufficientPile pile) player =
+  monadicIO $ do
     num <- randomRIO (1, length pile)
     let (topn, rest) = splitAt num pile
-    let (player', pile') = run . runState pile . execState player . evalState (DrawFlag False 0) $ draws num (const $ return (PlayerDrawCards (player ^. index) num))
-    return . label "drawsCorrectlyDrawNCardsIntoPlayerCandidates" $ (player' ^. candidates & take num) == topn && rest == pile'
+    let (player', pile') =
+          run . runState pile . execState player . evalState (DrawFlag False 0) $
+          draws num (const $ return (PlayerDrawCards (player ^. index) num))
+    return . label "drawsCorrectlyDrawNCardsIntoPlayerCandidates" $
+      (player' ^. candidates & take num) == topn && rest == pile'
 
 prop_drawsTurnOnDrawFlag :: AnyPileTest -> Player -> Property
-prop_drawsTurnOnDrawFlag (AnyPileTest pile) player = monadicIO $ do
+prop_drawsTurnOnDrawFlag (AnyPileTest pile) player =
+  monadicIO $ do
     num <- randomIO
-    let (DrawFlag b _) = run . evalState pile . evalState player . execState (DrawFlag False 0) $ draws num (const $ return (PlayerDrawCards (player ^. index) num))
+    let (DrawFlag b _) =
+          run . evalState pile . evalState player . execState (DrawFlag False 0) $
+          draws num (const $ return (PlayerDrawCards (player ^. index) num))
     return $ label "drawsTurnOnDrawFlag" b
 
 prop_drawsCorrectlyHandleNonsufficientPile :: AnyPileTest -> Player -> Property
 prop_drawsCorrectlyHandleNonsufficientPile (AnyPileTest pile) player =
-    let num = length pile + 1
-        ((report, player'), pile') = run . runState pile . runState player . evalState (DrawFlag False 0) $ draws num (const $ return (PlayerPass (player ^. index)))
-    in label "drawsCorrectlyHandleNonsufficientPile" $ (length pile - length pile') == (player' ^. candidates & length) - (player ^. candidates & length) && report == PlayerDrawCards (player ^. index) (length pile - length pile')
+  let num = length pile + 1
+      ((report, player'), pile') =
+        run . runState pile . runState player . evalState (DrawFlag False 0) $
+        draws num (const $ return (PlayerPass (player ^. index)))
+   in label "drawsCorrectlyHandleNonsufficientPile" $
+      (length pile - length pile') ==
+      (player' ^. candidates & length) - (player ^. candidates & length) &&
+      report == PlayerDrawCards (player ^. index) (length pile - length pile')
 
 prop_drawsCalledMultipleTimesAccumulate :: SufficientPile -> Player -> Property
 prop_drawsCalledMultipleTimesAccumulate (SufficientPile pile) player =
-    let (DrawFlag _ n) = run . evalState pile . evalState player . execState (DrawFlag False 0) $ draws half (const $ return (PlayerDrawCards (player ^. index) half)) >> draws half (const $ return (PlayerDrawCards (player ^. index) (half * 2)))
-    in label "drawsCalledMultipleTimesAccumulate" $ n == half * 2
-    where half = (length pile) `div` 2
+  let (DrawFlag _ n) =
+        run . evalState pile . evalState player . execState (DrawFlag False 0) $
+        draws half (const $ return (PlayerDrawCards (player ^. index) half)) >>
+        draws
+          half
+          (const $ return (PlayerDrawCards (player ^. index) (half * 2)))
+   in label "drawsCalledMultipleTimesAccumulate" $ n == half * 2
+  where
+    half = (length pile) `div` 2
 
 prop_playCorrectlyPlayCard :: CapablePlayer -> Property
-prop_playCorrectlyPlayCard (CapablePlayer player) = monadicIO $ do
-    card <- ((player ^. candidates) !!) <$> randomRIO (0, (player ^. candidates & length) - 1)
+prop_playCorrectlyPlayCard (CapablePlayer player) =
+  monadicIO $ do
+    card <-
+      ((player ^. candidates) !!) <$>
+      randomRIO (0, (player ^. candidates & length) - 1)
     color' <- randomIO
     let cardPlayed = randomChooseColor (card ^. runCardNew) color'
-        (((Right (PlayerPlayCard _ c), _), Just c'), player') = run . runState player . runFirstWriter @CardPlayed . runState (DrawFlag False 0) . runError @GameException $ play cardPlayed
-    return . label "playCorrectlyPlayCard" $ c == c' && (player' ^. candidates & (card :) & sort) == (player ^. candidates & sort)
+        (((Right (PlayerPlayCard _ c), _), Just c'), player') =
+          run .
+          runState player .
+          runFirstWriter @CardPlayed .
+          runState (DrawFlag False 0) . runError @GameException $
+          play cardPlayed
+    return . label "playCorrectlyPlayCard" $
+      c == c' &&
+      (player' ^. candidates & (card :) & sort) == (player ^. candidates & sort)
 
-prop_playShouldThrowErrorOnNonexistingCard :: IncapablePlayer -> Card Identity -> Property
-prop_playShouldThrowErrorOnNonexistingCard (IncapablePlayer player) randomCard = monadicIO $ do
-    let (((Left (PlayNonexistingCard (CardPlayed c)), _), Nothing), player') = run . runState player . runFirstWriter @CardPlayed . runState (DrawFlag False 0) . runError @GameException $ play (CardPlayed randomCard)
-    return . label "playShouldThrowErrorOnNonexistingCard" $ c == randomCard && player' == player
+prop_playShouldThrowErrorOnNonexistingCard ::
+     IncapablePlayer -> Card Identity -> Property
+prop_playShouldThrowErrorOnNonexistingCard (IncapablePlayer player) randomCard =
+  monadicIO $ do
+    let (((Left (PlayNonexistingCard (CardPlayed c)), _), Nothing), player') =
+          run .
+          runState player .
+          runFirstWriter @CardPlayed .
+          runState (DrawFlag False 0) . runError @GameException $
+          play (CardPlayed randomCard)
+    return . label "playShouldThrowErrorOnNonexistingCard" $
+      c == randomCard && player' == player
 
-prop_drawsAndPlayOutputPlayerDrawAndPlay :: SufficientPile -> AnyPlayer -> Property
-prop_drawsAndPlayOutputPlayerDrawAndPlay (SufficientPile pile) (AnyPlayer player) = monadicIO $ do
+prop_drawsAndPlayOutputPlayerDrawAndPlay ::
+     SufficientPile -> AnyPlayer -> Property
+prop_drawsAndPlayOutputPlayerDrawAndPlay (SufficientPile pile) (AnyPlayer player) =
+  monadicIO $ do
     color' <- randomIO
-    let (((Right (PlayerDrawAndPlay _ n c), _), Just c'), player') = run . runState player . runFirstWriter @CardPlayed . runState (DrawFlag False 0) . runError @GameException . evalState pile $ do
-                                                                    draws 1 $ \[card] ->
-                                                                        play (randomChooseColor (coerce card) color')
+    let (((Right (PlayerDrawAndPlay _ n c), _), Just c'), player') =
+          run .
+          runState player .
+          runFirstWriter @CardPlayed .
+          runState (DrawFlag False 0) . runError @GameException . evalState pile $ do
+            draws 1 $ \[card] -> play (randomChooseColor (coerce card) color')
     return . label "drawsAndPlayOutputPlayerDrawAndPlay" $ n == 1 && c == c'
 
 prop_initialPileMeetsRequirements :: Property
 prop_initialPileMeetsRequirements =
-    label "initialPileMeetsRequirements" $ length initialPile == 108
-        && length (filter isNumber initialPile) == 76
-        && length (filter isSpell initialPile) == 24
-        && length (filter isUniversal initialPile) == 8
-    where isNumber (CardNewKindView (Number _)) = True
-          isNumber _                            = False
-          isSpell (CardNewKindView (Spell Draw2))   = True
-          isSpell (CardNewKindView (Spell Skip))    = True
-          isSpell (CardNewKindView (Spell Reverse)) = True
-          isSpell _                                 = False
-          isUniversal (CardNewKindView (Spell Draw4))     = True
-          isUniversal (CardNewKindView (Spell Universal)) = True
-          isUniversal _                                   = False
+  label "initialPileMeetsRequirements" $
+  length initialPile == 108 &&
+  length (filter isNumber initialPile) == 76 &&
+  length (filter isSpell initialPile) == 24 &&
+  length (filter isUniversal initialPile) == 8
+  where
+    isNumber (CardNewKindView (Number _)) = True
+    isNumber _                            = False
+    isSpell (CardNewKindView (Spell Draw2))   = True
+    isSpell (CardNewKindView (Spell Skip))    = True
+    isSpell (CardNewKindView (Spell Reverse)) = True
+    isSpell _                                 = False
+    isUniversal (CardNewKindView (Spell Draw4))     = True
+    isUniversal (CardNewKindView (Spell Universal)) = True
+    isUniversal _                                   = False
 
 randomChooseColor :: Card Maybe -> Color -> CardPlayed
-randomChooseColor c@(view color -> Nothing) color' = c & color .~ (return color') & CardPlayed
+randomChooseColor c@(view color -> Nothing) color' =
+  c & color .~ (return color') & CardPlayed
 randomChooseColor c _ = c & color %~ (return . fromJust) & CardPlayed
 
 main :: IO ()
 main = do
-    quickCheck prop_drawsCorrectlyDrawNCardsIntoPlayerCandidates
-    quickCheck prop_drawsTurnOnDrawFlag
-    quickCheck prop_drawsCorrectlyHandleNonsufficientPile
-    quickCheck prop_drawsCalledMultipleTimesAccumulate
-    quickCheck prop_playCorrectlyPlayCard
-    quickCheck prop_playShouldThrowErrorOnNonexistingCard
-    quickCheck prop_drawsAndPlayOutputPlayerDrawAndPlay
-    quickCheck prop_initialPileMeetsRequirements
+  quickCheck prop_drawsCorrectlyDrawNCardsIntoPlayerCandidates
+  quickCheck prop_drawsTurnOnDrawFlag
+  quickCheck prop_drawsCorrectlyHandleNonsufficientPile
+  quickCheck prop_drawsCalledMultipleTimesAccumulate
+  quickCheck prop_playCorrectlyPlayCard
+  quickCheck prop_playShouldThrowErrorOnNonexistingCard
+  quickCheck prop_drawsAndPlayOutputPlayerDrawAndPlay
+  quickCheck prop_initialPileMeetsRequirements
